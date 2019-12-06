3f1e28d06f43996d58733648146338bae2047bd4
mksninja <kitavaurod@gmail.com>
2019-12-06 21:53:53 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter,Switch,Route } from 'react-router-dom'
+import { HashRouter,Switch,Route } from 'react-router-dom'
 import { TodosPage } from './components/pages/TodosPage';
 import { AboutPage } from './components/pages/AboutPage';
 
 const App: React.FC = () => {
   return <>
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
   <Navbar />
    <div className="container">
   <Switch>
@@ -14,7 +14,7 @@ const App: React.FC = () => {
     <Route component={AboutPage} path="/about"/>
   </Switch>
    </div>
-  </BrowserRouter> 
+  </HashRouter> 
   </>
 }
 
